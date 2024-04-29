5f1b70752cc1f213b8ed1e467a39241cb63bebe7
Mathis Telle <tellemathis@gmail.com>
2024-04-29 15:24:51 +0200

    opt 1 modif kernel

--- demo/kernel.c
+++ demo/kernel.c
@@ -1,19 +1,34 @@
 #ifdef OPT1
 
-/* Removing of store to load dependency (array ref replaced by scalar) */
-void kernel (unsigned n, float a[n][n], float b[n][n], float c[n][n]) {
-   int i, j, k;
+void kernel (unsigned n, const float a[n], const float b[n], float c[n][n]) {
+   
+  unsigned i, j;
 
-   for (i=0; i<n; i++)
-      for (j=0; j<n; j++) {
-         float s = 0.0f;
+  // Précalculer les exponentielles de a et b
+  float exp_a[n], exp_b[n];
+  for (i = 0; i < n; i++) {
+      exp_a[i] = exp(a[i]);
+      exp_b[i] = exp(b[i]);
+  }
 
-         for (k=0; k<n; k++)
-            s += a[i][k] * b[k][j];
-
-         c[i][j] = s;
+  // Initialiser le tableau c à zéro
+  for (i = 0; i < n; i++) {
+      for (j = 0; j < n; j++) {
+          c[i][j] = 0.0;
       }
+  }
+
+  // Remplir le tableau c en utilisant les valeurs précalculées
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+       if (j == 0)
+          c[i][0] += exp_a[i] * exp_b[j];
+       else
+          c[i][j] += fmin(c[i][0], exp_a[i] * exp_b[j]);
+    } 
+  }  
 }
+
 
 #elif defined OPT2
 
